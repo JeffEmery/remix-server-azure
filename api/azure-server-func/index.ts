8fee9be69ea5d343c9b3bee696e393eca8a9e5cd
Jeff Emery <jemery@envisionradiology.com>
2022-05-16 19:33:05 -0600

    test 3 change build label

--- api/azure-server-func/index.ts
+++ api/azure-server-func/index.ts
@@ -8,7 +8,7 @@ const httpTrigger: AzureFunction = async function (
   const name = req.query.name || (req.body && req.body.name)
   const responseMessage = name
     ? 'Hello, ' + name + '. This HTTP triggered function executed successfully.'
-    : 'Deploy Test 2: This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.'
+    : 'Deploy Test 3: This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.'
 
   context.res = {
     // status: 200, /* Defaults to 200 */
