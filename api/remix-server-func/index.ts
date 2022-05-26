4dbbe7cd129272cb6616fc5660f2d0c3953ad761
Jeff Emery <jemery@envisionradiology.com>
2022-05-26 17:56:00 -0600

    updating api remix modules

--- api/remix-server-func/index.ts
+++
@@ -1,8 +0,0 @@
-import './globals'
-import { createRequestHandler } from './server'
-
-export type { GetLoadContextFunction, RequestHandler } from './server'
-
-//export { createRequestHandler } from './server'
-
-module.exports = createRequestHandler({ build: require('../../build') })
